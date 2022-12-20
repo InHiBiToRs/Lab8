1de98d9e7ac70409c6f21332aa256019880d1509
InHiBiToRs <tanakrit64566@gmail.com>
2022-12-20 16:28:58 +0700

    lab8_3_complete

--- lab8_3.cpp
+++ lab8_3.cpp
@@ -2,7 +2,19 @@
 using namespace std;
 
 char before(char x){
-	//Write your function definition here
+	char y;
+	if(x > (char)64 and x < (char)91){
+		y = x-1;
+	}else{
+		y = '0';
+	}
+	if(x == 'A'){
+		y = 'Z';
+	}
+	
+	return y;
+
+
 }
 
 int main(){
